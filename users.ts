af32a73a9b94c7f4da460459421fd29bc8a58c09
Leonardo Palamim Cardozo <leopalamim@gmail.com>
2026-06-17 08:39:19 -0300

    added gender to example users

--- users.ts
+++ users.ts
@@ -2,10 +2,22 @@ export interface User {
   id: string;
   name: string;
   emailAddress: string;
+  gender: 'Male' | 'Female';
 }
 
 const users: Record<string, User> = {
-  u_123: { id: 'u_123', name: 'Ada', emailAddress: 'ada@example.com' },
+  u_123: {
+    id: 'u_123',
+    name: 'Ada',
+    emailAddress: 'ada@example.com',
+    gender: 'Female',
+  },
+  u_321: {
+    id: 'u_321',
+    name: 'John',
+    emailAddress: 'john@example.com',
+    gender: 'Male',
+  },
 };
 
 export function getUser(id: string): User | undefined {
